95c9ddd553d6f465a0a5414fd0a9304287ab579e
Cactus <tanya.bubnova.94@mail.ru>
2024-03-06 14:38:12 +0300

    Добавьте файлы проекта.

---
+++ RailWayUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RailWayUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
